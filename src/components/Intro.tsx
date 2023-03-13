34f8c34b92c154c5e1df489c593191361f179efd
Hazeeq <hazeeqzainal@gmail.com>
2023-03-13 12:17:56 +0800

    WIP intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,18 +5,7 @@ function Intro() {
       <p className="text-base md:text-xl mb-3 font-medium">
         Fullstack Web Developer
       </p>
-      <p className="text-sm max-w-xl mb-6 font-bold">
-        I am a skilled Fullstack Web Developer with 2 years of experience at DBS
-        in the Fraud Detection and Prevention Systems (FDPS) team. My expertise
-        in Angular and Spring Boot has allowed me to create scalable
-        applications that meet complex business requirements.
-        <br />I am dedicated to delivering efficient and effective solutions and
-        thrive in fast-paced, collaborative environments. With excellent
-        communication skills and a commitment to excellence, I am a valuable
-        asset to any development team. I stay current with the latest web
-        development trends and am currently exploring React, a powerful
-        JavaScript library for building dynamic user interfaces.
-      </p>
+      <p className="text-sm max-w-xl mb-6 font-bold">WIP</p>
     </div>
   );
 }
