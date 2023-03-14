d4c48510836648a471235b0fd79076705ac50ed1
Hazeeq <hazeeqzainal@gmail.com>
2023-03-15 01:02:38 +0800

    Added contact form

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
       <Intro />
       <Portfolio />
       <Timeline />
+      <Contact />
     </div>
   );
 }
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,40 @@
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="flex flex-col mb-10 mx-auto">
+      <div className="flex justify-center items-center">
+        <form
+          action="https://getform.io/f/692274b5-fe67-46d0-b694-d8097839f506"
+          method="POST"
+          className="flex flex-col w-full md:w-7/12"
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"
+          />
+          <textarea
+            name=""
+            placeholder="Message"
+            rows={10}
+            className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
+          />
+          <button
+            type="button"
+            className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-blue-700 to-green-300 drop-shadow-md hover:stroke-white"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
